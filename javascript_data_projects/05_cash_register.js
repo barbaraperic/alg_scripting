6bd23da01fe243904ec944eebef311952e999663
Barbara Peric <b.peric10@gmail.com>
2021-02-28 09:33:34 +0100

    cash register

--- javascript_data_projects/05_cash_register.js
+++ javascript_data_projects/05_cash_register.js
@@ -1,61 +1,77 @@
+var denom = [
+  { name: "ONE HUNDRED", val: 100.0 },
+  { name: "TWENTY", val: 20.0 },
+  { name: "TEN", val: 10.0 },
+  { name: "FIVE", val: 5.0 },
+  { name: "ONE", val: 1.0 },
+  { name: "QUARTER", val: 0.25 },
+  { name: "DIME", val: 0.1 },
+  { name: "NICKEL", val: 0.05 },
+  { name: "PENNY", val: 0.01 }
+];
+
+
 function checkCashRegister(price, cash, cid) {
 
-  function caluculateCashState(cid) {
-    const cashReg = cid.reduce((obj, item) => {
-      return {
-        ...obj,
-        [item[0]]: item[1]
-      }
-    }, {})
+  let output = { status: null, change: []}
 
-    const total = Object.values(cashReg).reduce((sum, num) => sum + num, 0)
+  let change = cash - price
 
-  return Number(total.toFixed(2))
+  var register = cid.reduce(
+    function(acc, curr) {
+      acc.total += curr[1];
+      acc[curr[0]] = curr[1];
+      return acc;
+    },
+    { total: 0 }
+  );
+
+  if (register.total === change) {
+    output.status = "CLOSED"
+    output.change = cid
+    return output
   }
 
-const value = {
-  "PENNY": 0.01,
-  "NICKEL": 0.05,
-  "DIME": 0.1,
-  "QUARTER": 0.25,
-  "DOLLAR": 1,
-  "FIVE": 5,
-  "TEN": 10,
-  "TWENTY": 20,
-  "ONE HUNDRED": 100
-}
+  if (register.total < change) {
+    output.status = "INSUFFICIENT_FUNDS"
+    return output
+  }
 
+console.log(register)
 
-
-  const cashState = caluculateCashState(cid)
-  const diff = cash - price
-
-  function calculateChange(diff) {
-    //diff = 5.25
-    let moneyValue = value
-    cid.map(item => item[1])
+  // Loop through the denomination array
+  var change_arr = denom.reduce(function(acc, curr) {
+    var value = 0;
     
-  }
+    while (register[curr.name] > 0 && change >= curr.val) {
+      change -= curr.val;
+      register[curr.name] -= curr.val;
+      value += curr.val;
 
-  const status = {
-    negative: "INSUFFICIENT_FUNDS",
-    zero: "CLOSED",
-    positive: "OPEN"
-  }
-
-
-
-  if (cashState < diff) {
-    return {
-      status: status.negative,
-      change: []
+      change = Math.round(change * 100) / 100;
     }
+    // Add this denomination to the output only if any was used.
+    if (value > 0) {
+      acc.push([curr.name, value]);
+    }
+    return acc; // Return the current change_arr
+  }, []); // Initial value of empty array for reduce
+
+
+  if (change_arr.length < 1 || change > 0) {
+    output.status = "INSUFFICIENT_FUNDS";
+    return output;
   }
 
-  return change;
+  // Here is your change, ma'am.
+  output.status = "OPEN";
+  output.change = change_arr;
+  return output;
+
+
 }
 
-checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
+checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
 
 
 //Code Explanation
