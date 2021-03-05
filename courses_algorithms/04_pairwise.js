09a55143d5c4c9b259b38f2e7c5c340512704745
Barbara Peric <b.peric10@gmail.com>
2021-03-05 10:58:15 +0100

    pairwise

---
+++ courses_algorithms/04_pairwise.js
@@ -0,0 +1,19 @@
+function pairwise(arr, arg) {
+  let pairIndices = [];
+  // Check every pair
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      // Exclude pairs that contain previously paired elements
+      if (arr[i] + arr[j] == arg
+          && !pairIndices.includes(i)
+          && !pairIndices.includes(j)) {
+        pairIndices.push(i, j);
+        break;
+      }
+    }
+  }
+
+  return pairIndices.reduce((sum, curr, index) => sum + curr, 0);
+}
+
+pairwise([1,4,2,3,0,5], 7);
