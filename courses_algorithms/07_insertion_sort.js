f4ec835e4a965ceeb0380639fb0b1604d42db91e
Barbara Peric <b.peric10@gmail.com>
2021-03-10 09:38:19 +0100

    insertion sort

---
+++ courses_algorithms/07_insertion_sort.js
@@ -0,0 +1,21 @@
+function insertionSort(array) {
+  for (let i = 1; i < array.length; i++ ) {
+    
+    for (let j = i; j > 0; j-- ) {
+      if (array[j] < array[j - 1]) {
+        let temp = array[j];
+        array[j] = array[j - 1];
+        array[j - 1] = temp;
+      } else {
+        break
+      }
+    }
+  }
+  return array
+}
+
+
+
+insertionSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]);
+
+// [1, 2, 4] 8
