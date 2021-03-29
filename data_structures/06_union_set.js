f8d7011d8aa09e82a62029c8a3dcfa39d735f3d6
Barbara Peric <b.peric10@gmail.com>
2021-03-29 09:29:32 +0200

    union set

---
+++ data_structures/06_union_set.js
@@ -0,0 +1,52 @@
+class Set {
+  constructor() {
+    // This will hold the set
+    this.dictionary = {};
+    this.length = 0;
+  }
+  // This method will check for the presence of an element and return true or false
+  has(element) {
+    return this.dictionary[element] !== undefined;
+  }
+  // This method will return all the values in the set
+  values() {
+    return Object.values(this.dictionary);
+  }
+  // This method will add an element to the set
+  add(element) {
+    if (!this.has(element)) {
+      this.dictionary[element] = element;
+      this.length++;
+      return true;
+    }
+
+    return false;
+  }
+  // This method will remove an element from a set
+  remove(element) {
+    if (this.has(element)) {
+      delete this.dictionary[element];
+      this.length--;
+      return true;
+    }
+
+    return false;
+  }
+  // This method will return the size of the set
+  size() {
+    return this.length;
+  }
+  union(otherSet) {
+    const unionSet = new Set();
+    const firstSet = this.values();
+    const secondSet = otherSet.values();
+    firstSet.forEach(e => {
+      unionSet.add(e)
+    })
+    secondSet.forEach(e => {
+      unionSet.add(e)
+    })
+    return unionSet
+
+  }
+}
