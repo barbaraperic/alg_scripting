c463cdfd240ffd63a2e9e36530917a7909358948
Barbara Peric <b.peric10@gmail.com>
2021-04-01 10:41:07 +0200

    spread operator

---
+++ data_structures/13_es6_set_spread.js
@@ -0,0 +1,15 @@
+function checkSet(set){
+  // Only change code below this line
+  return [...set]
+  // Only change code above this line
+}
+
+// let set = new Set();
+// let john = { name: "John" };
+// set.add(john)
+// Set(1) {{…}}[[Entries]]0: value: {name: "John"}
+// let arr = [1, 2, 3]
+// set.add(arr)
+// Set(2) {{…}, Array(3)}[[Entries]]0: Object1: Array(3)value: (3) [1, 2, 3]size: (...)__proto__: Set
+// [...set]
+// (2) [{…}, Array(3)]0: {name: "John"}1: (3) [1, 2, 3]length: 2__proto__: Array(0)
