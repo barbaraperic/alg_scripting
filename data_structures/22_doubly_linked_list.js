a8dabefebe0b2d32549d5c576467d0ba0ef45484
Barbara Peric <b.peric10@gmail.com>
2021-04-11 10:30:55 +0200

    remove node in doubly linked list

--- data_structures/22_doubly_linked_list.js
+++ data_structures/22_doubly_linked_list.js
@@ -20,5 +20,25 @@ var DoublyLinkedList = function() {
     }
   }
 
+  this.remove = function (data) {
+    if (this.head === null) return null;
+    let tempNode = this.head;
+    while (tempNode !== this.tail) {
+      if (tempNode.data === data) {
+        if (tempNode === this.head) {
+          this.head = tempNode.next;
+          tempNode.next.prev = null;
+        } else {
+          let prevNode = tempNode.prev;
+          prevNode.next = tempNode.next;
+        }
+      }
+      tempNode = tempNode.next;
+    }
+    if (tempNode.data === data) {
+      this.tail = tempNode.prev;
+      tempNode.prev.next = null;
+    }
+  };
   // Only change code above this line
 };
