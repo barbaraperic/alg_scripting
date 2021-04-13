372bd3fd0614c2180f5f57e883dd9ec04b74877f
Barbara Peric <b.peric10@gmail.com>
2021-04-13 11:09:25 +0200

    binary tree

--- data_structures/22_doubly_linked_list.js
+++ data_structures/22_doubly_linked_list.js
@@ -21,24 +21,43 @@ var DoublyLinkedList = function() {
   }
 
   this.remove = function (data) {
-    if (this.head === null) return null;
-    let tempNode = this.head;
-    while (tempNode !== this.tail) {
-      if (tempNode.data === data) {
-        if (tempNode === this.head) {
-          this.head = tempNode.next;
-          tempNode.next.prev = null;
-        } else {
-          let prevNode = tempNode.prev;
-          prevNode.next = tempNode.next;
+    if (!this.head) {
+      return null;
+    }
+
+    let temp = this.head;
+
+    while(temp !== this.tail) {
+      if(temp.data === data) {
+        if(temp === this.head) {
+          head = temp.next;
+          temp.next.prev = null;
         }
       }
-      tempNode = tempNode.next;
     }
-    if (tempNode.data === data) {
-      this.tail = tempNode.prev;
-      tempNode.prev.next = null;
-    }
+
+
+
+
+
+    // if (this.head === null) return null;
+    // let tempNode = this.head;
+    // while (tempNode !== this.tail) {
+    //   if (tempNode.data === data) {
+    //     if (tempNode === this.head) {
+    //       this.head = tempNode.next;
+    //       tempNode.next.prev = null;
+    //     } else {
+    //       let prevNode = tempNode.prev;
+    //       prevNode.next = tempNode.next;
+    //     }
+    //   }
+    //   tempNode = tempNode.next;
+    // }
+    // if (tempNode.data === data) {
+    //   this.tail = tempNode.prev;
+    //   tempNode.prev.next = null;
+    // }
   };
   // Only change code above this line
 };
