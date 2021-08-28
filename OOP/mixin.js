ac22c2f13e90ebd79118dd20b29b85a98b486ed0
Barbara Peric <b.peric10@gmail.com>
2021-08-28 15:09:38 +0200

    mixin

---
+++ OOP/mixin.js
@@ -0,0 +1,20 @@
+let bird = {
+  name: "Donald",
+  numLegs: 2
+};
+
+let boat = {
+  name: "Warrior",
+  type: "race-boat"
+};
+
+// Only change code below this line
+
+let glideMixin = (obj) => {
+  obj.glide = () => {
+
+  }
+}
+
+glideMixin(bird)
+glideMixin(boat)
